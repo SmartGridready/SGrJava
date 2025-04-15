4fedd48543b1031a68027503fd216066e679509f
Matthias Krebs <matthias.krebs@ateleris.ch>
2025-04-15 10:43:49 +0200

    enabled dynamic tariff test which also tests dynamic request parameters.

--- CommHandler/src/test/java/com/smartgridready/communicator/rest/impl/TariffProvidersTest.java
+++ CommHandler/src/test/java/com/smartgridready/communicator/rest/impl/TariffProvidersTest.java
@@ -3,17 +3,17 @@ package com.smartgridready.communicator.rest.impl;
 import com.smartgridready.communicator.common.api.SGrDeviceBuilder;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled("uses real tariff providers, might be unstable")
 class TariffProvidersTest {
 
     static final Logger LOG = LoggerFactory.getLogger(TariffProvidersTest.class);
@@ -25,38 +25,55 @@ class TariffProvidersTest {
 
         var properties = new Properties();
         properties.put("token", "19d6ca0bb9bf4d8b6525440eead80da6");
-        properties.put("start_timestamp", "2024-01-01T00:00:00+02:00");
-        properties.put("end_timestamp", "2024-01-01T01:00:00+02:00");
         properties.put("metering_code", "CH1018601234500000000000000011642");
 
+        var dynamicParameters = new Properties();
+        dynamicParameters.put("start_timestamp", "2025-01-01T00:00:00+01:00");
+        dynamicParameters.put("end_timestamp", "2025-01-01T01:00:00+01:00");
+
         var device = new SGrDeviceBuilder()
-                .eid(Path.of(XML_BASE_DIR + "SGr_05_mmmm_dddd_Dynamic_Tariffs_Swisspower_V0.0.1.xml"))
+                .eid(Path.of(XML_BASE_DIR + "SGr_05_mmmm_dddd_Dynamic_Tariffs_Swisspower_V0.0.2.xml"))
                 .properties(properties)
                 .build();
+        
+        var dynamicRequestParameters = device
+                .getDataPoint("DynamicTariff", "TariffSupply")
+                .getDynamicRequestParameters()
+                .stream()
+                .collect(Collectors.toMap(e -> e.getName(), e -> e));
+        assertTrue(dynamicRequestParameters.containsKey("start_timestamp"));
+        assertTrue(dynamicRequestParameters.containsKey("end_timestamp"));
 
-        var result = device.getVal("DynamicTariff", "TariffSupply");
+        var result = device.getVal("DynamicTariff", "TariffSupply", dynamicParameters);
         LOG.info(result.getString());
 
-        String expected = "[{\"start_timestamp\":\"2024-01-01T00:00:00";
+        String expected = "[{\"start_timestamp\":\"2025-01-01T00:00:00";
         assertThat(result.getString(), CoreMatchers.startsWith(expected));
     }
 
     @Test
     void testGroupeE() throws Exception {
 
-        var properties = new Properties();
-        properties.put("start_timestamp", "2024-01-01T00:00:00+02:00");
-        properties.put("end_timestamp", "2024-01-02T00:00:00+02:00");
+        var dynamicParameters = new Properties();
+        dynamicParameters.put("start_timestamp", "2025-01-01T00:00:00+01:00");
+        dynamicParameters.put("end_timestamp", "2025-01-02T00:00:00+01:00");
 
         var device = new SGrDeviceBuilder()
-                .eid(Path.of(XML_BASE_DIR + "SGr_05_mmmm_dddd_Dynamic_Tariffs_GroupeE_V0.0.1.xml"))
-                .properties(properties)
+                .eid(Path.of(XML_BASE_DIR + "SGr_05_mmmm_dddd_Dynamic_Tariffs_GroupeE_V0.0.2.xml"))
                 .build();
 
-        var result = device.getVal("DynamicTariff", "TariffSupply");
+        var dynamicRequestParameters = device
+                .getDataPoint("DynamicTariff", "TariffSupply")
+                .getDynamicRequestParameters()
+                .stream()
+                .collect(Collectors.toMap(e -> e.getName(), e -> e));
+        assertTrue(dynamicRequestParameters.containsKey("start_timestamp"));
+        assertTrue(dynamicRequestParameters.containsKey("end_timestamp"));
+
+        var result = device.getVal("DynamicTariff", "TariffSupply", dynamicParameters);
         LOG.info(result.getString());
 
         assertThat(result.getString(),
-                CoreMatchers.startsWith("[{\"start_timestamp\":\"2023-12-31T23:00:00"));
+                CoreMatchers.startsWith("[{\"start_timestamp\":\"2025-01-01T00:00:00"));
     }
 }
